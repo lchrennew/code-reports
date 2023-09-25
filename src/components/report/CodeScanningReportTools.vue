b5954bcc202df89d88a8f822d052dcd738df90db
lichun <lchrennew@126.com>
2023-09-25 11:46:35 +0800

    feat: 批量工具条布局

--- src/components/report/CodeScanningReportTools.vue
+++ src/components/report/CodeScanningReportTools.vue
@@ -3,9 +3,26 @@
 </script>
 
 <template>
-    <div class="report-tools">工具条</div>
+    <div class="report-tools">
+        <a-space>
+            <a-button>全选</a-button>
+            <a-button>取消选择</a-button>
+            <a-button>反选</a-button>
+        </a-space>
+
+        <a-space>
+            <a-button>忽略所选</a-button>
+            <a-button>忽略全部</a-button>
+        </a-space>
+    </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="less">
+.report-tools {
+    padding: 1em;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+}
 </style>
